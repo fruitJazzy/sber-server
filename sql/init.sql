c38be15d782458c6af20f38c0e6adbb327132595
Khristian Chernetsov <k.chernetsov@i-dgtl.ru>
2017-07-10 18:27:00 +0300

    added init sql

---
+++ sql/init.sql
@@ -0,0 +1,18 @@
+CREATE TABLE PUBLIC.account
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    amount DOUBLE DEFAULT 0
+);
+CREATE UNIQUE INDEX account_id_uindex ON PUBLIC.account (id);
+
+CREATE TABLE PUBLIC.transactions
+(
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    number_from INT NOT NULL,
+    number_to INT NOT NULL,
+    transaction_timestamp TIMESTAMP,
+    amount DOUBLE
+);
+CREATE UNIQUE INDEX table_name_id_uindex ON PUBLIC.transactions (id);
